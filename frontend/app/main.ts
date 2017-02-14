bcd3a4f5f6db564182c249b6637da85a1d01f58b
Marvin König <kingmarv@users.noreply.github.com>
2017-02-14 16:52:03 +0100

    - added new angular version

--- frontend/app/main.ts
+++
@@ -1,5 +0,0 @@
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-import { AppModule } from './modules/app.module';
-
-const platform = platformBrowserDynamic();
-platform.bootstrapModule(AppModule);
