7b0c81e86b50a93285a9bab9ad067268c2001842
Marvin König <kingmarv@users.noreply.github.com>
2017-02-17 19:50:28 +0100

    - Routing - Juggling components (SignIn, PageNotFound)

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,18 +3,46 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { MaterialModule } from '@angular/material';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { SignInComponent } from './sign-in/sign-in.component';
+import { PageNotFoundComponent } from './errors/page-not-found/page-not-found.component';
+
+const appRoutes: Routes = [
+  {
+    path: 'sign-in',
+    component: SignInComponent
+  },
+  // { path: 'hero/:id',      component: HeroDetailComponent },
+  // {
+  //   path: 'heroes',
+  //   component: HeroListComponent,
+  //   data: { title: 'Heroes List' }
+  // },
+  {
+    path: '',
+    redirectTo: '/sign-in',
+    pathMatch: 'full'
+  },
+  {
+    path: '**',
+    component: PageNotFoundComponent
+  }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignInComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule,
-    MaterialModule.forRoot()
+    MaterialModule.forRoot(),
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
