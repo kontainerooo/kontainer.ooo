c8fdf0e57ad920d7f9e70740114467b4d4ce4838
Marvin König <kingmarv@users.noreply.github.com>
2017-02-27 14:55:59 +0100

    Make sidebar navigation dynamic
    Removed the hard coded sidebar navigation in favor of a component array.

--- frontend/src/app/sidebar-navigation/sidebar-navigation.component.ts
+++ frontend/src/app/sidebar-navigation/sidebar-navigation.component.ts
@@ -1,13 +1,29 @@
 import { Component, OnInit } from '@angular/core';
 
+import { SideNavElement } from '../interfaces/side-nav-element';
+
 @Component({
   selector: 'kio-sidebar-navigation',
   templateUrl: './sidebar-navigation.component.html',
   styleUrls: ['./sidebar-navigation.component.scss']
 })
 export class SidebarNavigationComponent implements OnInit {
-  
-  constructor() { }
+  navigation: SideNavElement[];
+
+  constructor() {
+    this.navigation = [
+      {
+        title: "Dashboard",
+        icon: "dashboard",
+        route: "/dashboard"
+      },
+      {
+        title: "Containers",
+        icon: "view_module",
+        route: "/dashboard"
+      }
+    ]
+  }
 
   ngOnInit() {
   }
