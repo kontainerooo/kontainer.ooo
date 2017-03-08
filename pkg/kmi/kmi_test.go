f26f2c5070e9decd2ab3eba66ae210b0a997af43
Tom Burmeister <tobuto@users.noreply.github.com>
2017-03-08 15:52:31 +0100

    Add DB dependency

--- pkg/kmi/kmi_test.go
+++ pkg/kmi/kmi_test.go
@@ -2,6 +2,7 @@ package kmi_test
 
 import (
 	"github.com/ttdennis/kontainer.io/pkg/kmi"
+	"github.com/ttdennis/kontainer.io/pkg/testutils"
 
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
@@ -10,8 +11,16 @@ import (
 var _ = Describe("KMI", func() {
 	Describe("Create Service", func() {
 		It("Should create service", func() {
-			kmiService := kmi.NewService()
+			kmiService, err := kmi.NewService(testutils.NewMockDB())
+			Ω(err).ShouldNot(HaveOccurred())
 			Expect(kmiService).ToNot(BeZero())
+		})
+
+		It("Should return db error", func() {
+			db := testutils.NewMockDB()
+			db.SetError(1)
+			_, err := kmi.NewService(db)
+			Ω(err).Should(HaveOccurred())
 		})
 	})
 
--- pkg/kmi/service.go
+++ pkg/kmi/service.go
@@ -1,6 +1,8 @@
 // Package kmi provides functionality to handle kmi files
 package kmi
 
+import "github.com/ttdennis/kontainer.io/pkg/abstraction"
+
 // The Service interface describes the functions necessary for a KMI Service
 type Service interface {
 	// AddKMI resolves the path to a kmi file, extracts it and adds its contents to the database as a new kontainer module
@@ -16,7 +18,18 @@ type Service interface {
 	KMI() *[]KMDI
 }
 
-type service struct{}
+type dbAdapter interface {
+	abstraction.DBAdapter
+	AutoMigrate(...interface{}) error
+}
+
+type service struct {
+	db dbAdapter
+}
+
+func (s *service) InitializeDatabases() error {
+	return s.db.AutoMigrate(&KMI{})
+}
 
 func (s *service) AddKMI(path string) (uint, error) {
 	//TODO: implement
@@ -38,7 +51,16 @@ func (s *service) KMI() *[]KMDI {
 	return nil
 }
 
-// NewService returns a new instance of a KMIService
-func NewService() Service {
-	return &service{}
+// NewService creates a KMIService with necessary dependencies.
+func NewService(db dbAdapter) (Service, error) {
+	s := &service{
+		db: db,
+	}
+
+	err := s.InitializeDatabases()
+	if err != nil {
+		return nil, err
+	}
+
+	return s, nil
 }
