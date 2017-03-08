1f4da8fb8b4129a872cbe4d1bc39066746d53b47
Tom Burmeister <tobuto@users.noreply.github.com>
2017-03-08 15:47:50 +0100

    Add Datatype contents

--- pkg/kmi/datatype.go
+++ pkg/kmi/datatype.go
@@ -2,8 +2,28 @@ package kmi
 
 // The KMDI struct contains the displaying information for the frontend
 type KMDI struct {
+	Name        string
+	Version     string
+	Description string
+	Type        int
+}
+
+type frontendModule struct {
+	Template   string
+	parameters map[string]string
 }
 
 // The KMI struct is used to represent every information included in a kmi-file
 type KMI struct {
+	KMDI
+	Commands    map[string]string
+	Environment map[string]interface{}
+	Frontend    []frontendModule
+	Imports     []string
+	Interfaces  map[string]uint16
+}
+
+// TableName sets KMI's tablename
+func (KMI) TableName() string {
+	return "kontainer_module_information"
 }
