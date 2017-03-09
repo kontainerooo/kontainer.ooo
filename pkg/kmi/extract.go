8e34da4899cf8ee22cc5965bda0762db28948d0b
Tom Burmeister <tobuto@users.noreply.github.com>
2017-03-09 18:53:18 +0100

    Add functions to extract data from .kmi-files

---
+++ pkg/kmi/extract.go
@@ -0,0 +1,244 @@
+package kmi
+
+import (
+	"archive/tar"
+	"encoding/json"
+	"fmt"
+	"io"
+	"os"
+	"path"
+	"reflect"
+)
+
+type kmiContent struct {
+	module     []byte
+	frontend   []byte
+	interfaces []byte
+	env        []byte
+	cmd        []byte
+	imports    []byte
+}
+
+func extract(src string, k *kmiContent) error {
+	f, err := os.Open(src)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+
+	tarReader := tar.NewReader(f)
+	for {
+		header, err := tarReader.Next()
+		if err == io.EOF {
+			break
+		} else if err != nil {
+			return err
+		}
+
+		if header.Typeflag != tar.TypeReg {
+			continue
+		}
+
+		_, name := path.Split(header.Name)
+
+		data := make([]byte, header.Size)
+		_, err = tarReader.Read(data)
+		if err != nil {
+			return err
+		}
+
+		switch name {
+		case "module.json":
+			k.module = data
+		case "frontend.json":
+			k.frontend = data
+		case "interfaces.json":
+		case "if.json":
+			k.interfaces = data
+		case "env.json":
+			k.env = data
+		case "cmd.json":
+			k.cmd = data
+		case "imports.json":
+			k.imports = data
+		default:
+			continue
+		}
+	}
+	return nil
+}
+
+type moduleJSON struct {
+	Name        string
+	Version     string
+	Description string
+	Type        float64
+	Imports     interface{}
+	Frontend    interface{}
+	Interfaces  interface{}
+	Env         interface{}
+	Cmd         interface{}
+}
+
+func chooseSource(src interface{}, outsrc []byte, kind reflect.Kind, name string) error {
+	v := reflect.ValueOf(src).Elem().Elem()
+	k := v.Kind()
+	if k == reflect.String {
+		err := json.Unmarshal(outsrc, src)
+		if err != nil {
+			return err
+		}
+		v = reflect.ValueOf(src).Elem().Elem()
+		k = v.Kind()
+	}
+
+	if k != kind {
+		return fmt.Errorf("%s malformatted", name)
+	}
+
+	return nil
+}
+
+func extractStringMap(value reflect.Value, dst map[string]interface{}, restriction map[reflect.Kind]bool) error {
+	if value.Kind() != reflect.Map {
+		return fmt.Errorf("src aint no map bra")
+	}
+	keys := value.MapKeys()
+	for _, key := range keys {
+		element := value.MapIndex(key).Elem()
+		switch element.Kind() {
+		case reflect.String:
+			if restriction[reflect.String] {
+				return fmt.Errorf("unexpected string")
+			}
+			dst[key.String()] = element.String()
+		case reflect.Float64:
+			if restriction[reflect.Int] {
+				return fmt.Errorf("unexpected number")
+			}
+			dst[key.String()] = int(element.Float())
+		default:
+			return fmt.Errorf("unexpected %s", element.Kind().String())
+		}
+	}
+	return nil
+}
+
+func getStringMap(src interface{}, outsrc []byte, dst map[string]interface{}, name string, restriction map[reflect.Kind]bool) error {
+	err := chooseSource(&src, outsrc, reflect.Map, name)
+	if err != nil {
+		return err
+	}
+
+	err = extractStringMap(reflect.ValueOf(src), dst, restriction)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func getStringSlice(src interface{}, outsrc []byte, dst *[]string, name string) error {
+	err := chooseSource(&src, outsrc, reflect.Slice, name)
+	if err != nil {
+		return err
+	}
+
+	value := reflect.ValueOf(src)
+	len := value.Len()
+
+	for i := 0; i < len; i++ {
+		element := value.Index(i).Elem()
+		if element.Kind() != reflect.String {
+			return fmt.Errorf("unexpected %T", element)
+		}
+		*dst = append(*dst, element.String())
+	}
+
+	return nil
+}
+
+func getFrontend(src interface{}, outsrc []byte, dst *[]frontendModule) error {
+	err := chooseSource(&src, outsrc, reflect.Slice, "frontend")
+	if err != nil {
+		return err
+	}
+
+	value := reflect.ValueOf(src)
+	len := value.Len()
+	for i := 0; i < len; i++ {
+		fm := frontendModule{}
+		module := value.Index(i).Elem()
+		keys := module.MapKeys()
+		for i, key := range keys {
+			switch key.String() {
+			case "template":
+				tpl := module.MapIndex(key).Elem()
+				if tpl.Kind() != reflect.String {
+					return fmt.Errorf("template is not of type string in module %d", i)
+				}
+				fm.template = tpl.String()
+			case "parameters":
+				params := module.MapIndex(key).Elem()
+				if params.Kind() != reflect.Map {
+					return fmt.Errorf("parameters are no json in module %d", i)
+				}
+				fm.parameters = make(map[string]interface{})
+				err = extractStringMap(params, fm.parameters, nil)
+				if err != nil {
+					return err
+				}
+			default:
+				return fmt.Errorf("unsupported property %s", key.String())
+			}
+		}
+		*dst = append(*dst, fm)
+	}
+
+	return nil
+}
+
+func getData(kC *kmiContent, k *KMI) error {
+	m := &moduleJSON{}
+	err := json.Unmarshal(kC.module, m)
+	if err != nil {
+		return err
+	}
+
+	k.Name = m.Name
+	k.Version = m.Version
+	k.Description = m.Description
+	k.Type = int(m.Type)
+
+	intRestriction := make(map[reflect.Kind]bool)
+	intRestriction[reflect.Int] = true
+
+	k.Commands = make(map[string]interface{})
+	err = getStringMap(m.Cmd, kC.cmd, k.Commands, "commands", intRestriction)
+	if err != nil {
+		return err
+	}
+
+	k.Environment = make(map[string]interface{})
+	err = getStringMap(m.Env, kC.env, k.Environment, "environment", nil)
+	if err != nil {
+		return err
+	}
+
+	k.Interfaces = make(map[string]interface{})
+	err = getStringMap(m.Interfaces, kC.interfaces, k.Interfaces, "interfaces", nil)
+	if err != nil {
+		return err
+	}
+
+	err = getFrontend(m.Frontend, kC.frontend, &k.Frontend)
+	if err != nil {
+		return err
+	}
+
+	err = getStringSlice(m.Imports, kC.imports, &k.Imports, "imports")
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
