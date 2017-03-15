6ff12e8ac9fa7d7fccba47537c0b36d35245279b
Tom Burmeister <tobuto@users.noreply.github.com>
2017-03-15 09:31:24 +0100

    Add KMI Service WebSocket Transport

---
+++ pkg/kmi/transport_ws.go
@@ -0,0 +1,96 @@
+package kmi
+
+import (
+	"context"
+
+	"github.com/golang/protobuf/proto"
+	"github.com/ttdennis/kontainer.io/pkg/pb"
+	ws "github.com/ttdennis/kontainer.io/pkg/websocket"
+)
+
+// MakeWebsocketService makes a set of kmi Endpoints available as a websocket Service
+func MakeWebsocketService(endpoints Endpoints) *ws.ServiceDescription {
+	service := ws.NewServiceDescription("kmiService", ws.ProtoIDFromString("KMI"))
+
+	service.AddEndpoint(ws.NewServiceEndpoint(
+		"AddKMI",
+		ws.ProtoIDFromString("ADD"),
+		endpoints.AddKMIEndpoint,
+		DecodeWSAddKMIRequest,
+		EncodeGRPCAddKMIResponse,
+	))
+
+	service.AddEndpoint(ws.NewServiceEndpoint(
+		"RemoveKMI",
+		ws.ProtoIDFromString("REM"),
+		endpoints.RemoveKMIEndpoint,
+		DecodeWSRemoveKMIRequest,
+		EncodeGRPCRemoveKMIResponse,
+	))
+
+	service.AddEndpoint(ws.NewServiceEndpoint(
+		"GetKMI",
+		ws.ProtoIDFromString("GET"),
+		endpoints.GetKMIEndpoint,
+		DecodeWSGetKMIRequest,
+		EncodeGRPCGetKMIResponse,
+	))
+
+	service.AddEndpoint(ws.NewServiceEndpoint(
+		"KMI",
+		ws.ProtoIDFromString("ALL"),
+		endpoints.KMIEndpoint,
+		DecodeWSKMIRequest,
+		EncodeGRPCKMIResponse,
+	))
+
+	return service
+}
+
+// DecodeWSAddKMIRequest is a websocket.DecodeRequestFunc that converts a
+// WS AddKMI request to a messages/kmi.proto-domain addkmi request.
+func DecodeWSAddKMIRequest(ctx context.Context, data interface{}) (interface{}, error) {
+	req := &pb.AddKMIRequest{}
+	err := proto.Unmarshal(data.([]byte), req)
+	if err != nil {
+		return nil, err
+	}
+
+	return DecodeGRPCAddKMIRequest(ctx, req)
+}
+
+// DecodeWSRemoveKMIRequest is a websocket.DecodeRequestFunc that converts a
+// WS RemoveKMI request to a messages/kmi.proto-domain removekmi request.
+func DecodeWSRemoveKMIRequest(ctx context.Context, data interface{}) (interface{}, error) {
+	req := &pb.RemoveKMIRequest{}
+	err := proto.Unmarshal(data.([]byte), req)
+	if err != nil {
+		return nil, err
+	}
+
+	return DecodeGRPCRemoveKMIRequest(ctx, req)
+}
+
+// DecodeWSGetKMIRequest is a websocket.DecodeRequestFunc that converts a
+// WS GetKMI request to a messages/kmi.proto-domain getkmi request.
+func DecodeWSGetKMIRequest(ctx context.Context, data interface{}) (interface{}, error) {
+	req := &pb.GetKMIRequest{}
+	err := proto.Unmarshal(data.([]byte), req)
+	if err != nil {
+		return nil, err
+	}
+
+	return DecodeGRPCGetKMIRequest(ctx, req)
+}
+
+// DecodeWSKMIRequest is a websocket.DecodeRequestFunc that converts a
+// WS KMI request to a messages/kmi.proto-domain kmi request.
+func DecodeWSKMIRequest(ctx context.Context, data interface{}) (interface{}, error) {
+	req := &pb.KMIRequest{}
+	err := proto.Unmarshal(data.([]byte), req)
+	if err != nil {
+		return nil, err
+	}
+
+	return DecodeGRPCKMIRequest(ctx, req)
+}
