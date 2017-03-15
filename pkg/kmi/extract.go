cbe1888fc8ef1b5c486415cf275bcab1ba601f39
Tom Burmeister <tobuto@users.noreply.github.com>
2017-03-15 18:53:48 +0100

    Add usage of 'real' paths in kmi.Extract functions
    Until now, the Extract function could only detect files in hardcoded
    places. Now the extractor creates a datastructure containing every file
    in the kmi sorted by its name/path. The chooseSource function now
    checks for the filepath stored in the src rather than just accessing a
    fixed value.
    
    To implement this, several data types had to be added:
    - The Folder type contains two maps:
      - files, which maps a string (name) to a byte array (data)
      - folders, which is a FolderMap
    - The FolderMap type is a map mapping strings (names) to a Folder
    
    Furthermore the FolderMap type implements a AddFile and a GetFile
    function for data handling purposes. Those functions are wrapped in
    kmi.Contents Functions with the same name to provide KMI specific
    functionalities (prefixing paths, checking for the module.json).

--- pkg/kmi/extract.go
+++ pkg/kmi/extract.go
@@ -8,18 +8,105 @@ import (
 	"os"
 	"path"
 	"reflect"
+	"strings"
 
 	"github.com/lib/pq"
 )
 
+// Folder is a type used to abstract file hierarchie
+type Folder struct {
+	files   map[string]*[]byte
+	folders FolderMap
+}
+
+// FolderMap is a map mapping a string to a pointer of type folder
+type FolderMap map[string]*Folder
+
+func (f FolderMap) walk(dir string, create bool) (*Folder, error) {
+	var (
+		folder    *Folder
+		folderMap FolderMap
+	)
+	dirs := strings.Split(path.Clean(dir), "/")
+
+	folderMap = f
+	dirCount := len(dirs) - 1
+	for i, dir := range dirs {
+		_, exists := folderMap[dir]
+		if !exists {
+			if create {
+				folderMap[dir] = &Folder{
+					files:   make(map[string]*[]byte),
+					folders: make(FolderMap),
+				}
+			} else {
+				return nil, fmt.Errorf("folder %s does not exist", dir)
+			}
+		}
+		if i == dirCount {
+			folder = folderMap[dir]
+		} else {
+			folderMap = folderMap[dir].folders
+		}
+	}
+
+	return folder, nil
+}
+
+// AddFile adds a file in the given path with the given name to the Content
+func (f FolderMap) AddFile(p string, data *[]byte) {
+	dir, name := path.Split(p)
+	folder, _ := f.walk(dir, true)
+	folder.files[name] = data
+}
+
+// GetFile get a file in path p or an error
+func (f FolderMap) GetFile(p string) (*[]byte, error) {
+	dir, name := path.Split(p)
+	folder, err := f.walk(dir, false)
+	if err != nil {
+		return nil, err
+	}
+
+	file, ok := folder.files[name]
+	if !ok {
+		return nil, fmt.Errorf("file %s does not exist in %s", name, dir)
+	}
+
+	return file, nil
+}
+
 // Content is a struct type which may hold a byte array for every file which can be in the kmi file
 type Content struct {
 	Module     []byte
-	Frontend   []byte
-	Interfaces []byte
-	Env        []byte
-	Cmd        []byte
-	Imports    []byte
+	modulePath string
+	folders    FolderMap
+}
+
+// AddFile add a file to the FolderMap and set Module if applicable
+func (c *Content) AddFile(dir, file string, data *[]byte) {
+	c.folders.AddFile(path.Join(dir, file), data)
+	if file == "module.json" {
+		c.Module = *data
+		c.modulePath = dir
+	}
+}
+
+// GetFile add a file to the FolderMap and set Module if applicable
+func (c *Content) GetFile(p string) (*[]byte, error) {
+	dir, _ := path.Split(p)
+	dir = path.Clean(dir)
+	if dir == "" || dir == "." {
+		p = path.Join(c.modulePath, p)
+	}
+	return c.folders.GetFile(p)
+}
+
+// NewContent initializes a new Content instance
+func NewContent() *Content {
+	return &Content{
+		folders: make(FolderMap),
+	}
 }
 
 // Extract is used to get the data from a kmi tar ball
@@ -43,31 +130,13 @@ func Extract(src string, k *Content) error {
 			continue
 		}
 
-		_, name := path.Split(header.Name)
-
 		data := make([]byte, header.Size)
 		_, err = tarReader.Read(data)
 		if err != nil {
 			return err
 		}
-
-		switch name {
-		case "module.json":
-			k.Module = data
-		case "frontend.json":
-			k.Frontend = data
-		case "interfaces.json":
-		case "if.json":
-			k.Interfaces = data
-		case "env.json":
-			k.Env = data
-		case "cmd.json":
-			k.Cmd = data
-		case "imports.json":
-			k.Imports = data
-		default:
-			continue
-		}
+		dir, file := path.Split(header.Name)
+		k.AddFile(dir, file, &data)
 	}
 	return nil
 }
@@ -85,11 +154,16 @@ type moduleJSON struct {
 }
 
 // ChooseSource fills src with outsrc if src is not the expected data kind
-func ChooseSource(src interface{}, outsrc []byte, kind reflect.Kind, name string) error {
+func ChooseSource(src interface{}, outsrc *Content, kind reflect.Kind, name string) error {
 	v := reflect.ValueOf(src).Elem().Elem()
 	k := v.Kind()
 	if k == reflect.String {
-		err := json.Unmarshal(outsrc, src)
+		data, err := outsrc.GetFile(v.String())
+		if err != nil {
+			return err
+		}
+
+		err = json.Unmarshal(*data, src)
 		if err != nil {
 			return err
 		}
@@ -133,7 +207,7 @@ func ExtractStringMap(value reflect.Value, dst map[string]interface{}, restricti
 
 // GetStringMap takes a src and an outsrc, a destination map and a restriction map, decides which source to use to fill
 // the destination map
-func GetStringMap(src interface{}, outsrc []byte, dst map[string]interface{}, name string, restriction map[reflect.Kind]bool) error {
+func GetStringMap(src interface{}, outsrc *Content, dst map[string]interface{}, name string, restriction map[reflect.Kind]bool) error {
 	err := ChooseSource(&src, outsrc, reflect.Map, name)
 	if err != nil {
 		return err
@@ -148,7 +222,7 @@ func GetStringMap(src interface{}, outsrc []byte, dst map[string]interface{}, na
 
 // GetStringSlice takes a src and an outsrc and a destination slice, decides which source to use and to fill
 // the destination slice
-func GetStringSlice(src interface{}, outsrc []byte, dst *pq.StringArray, name string) error {
+func GetStringSlice(src interface{}, outsrc *Content, dst *pq.StringArray, name string) error {
 	err := ChooseSource(&src, outsrc, reflect.Slice, name)
 	if err != nil {
 		return err
@@ -170,7 +244,7 @@ func GetStringSlice(src interface{}, outsrc []byte, dst *pq.StringArray, name st
 
 // GetFrontend takes a src and an outsrc and a destination frontendModule slice, decides which source to use and extracts
 // the frontendModule specific information to fill the destination slice
-func GetFrontend(src interface{}, outsrc []byte, dst *FrontendArray) error {
+func GetFrontend(src interface{}, outsrc *Content, dst *FrontendArray) error {
 	err := ChooseSource(&src, outsrc, reflect.Slice, "frontend")
 	if err != nil {
 		return err
@@ -227,29 +301,29 @@ func GetData(kC *Content, k *KMI) error {
 	intRestriction[reflect.Int] = true
 
 	k.Commands = make(map[string]interface{})
-	err = GetStringMap(m.Cmd, kC.Cmd, k.Commands, "commands", intRestriction)
+	err = GetStringMap(m.Cmd, kC, k.Commands, "commands", intRestriction)
 	if err != nil {
 		return err
 	}
 
 	k.Environment = make(map[string]interface{})
-	err = GetStringMap(m.Env, kC.Env, k.Environment, "environment", nil)
+	err = GetStringMap(m.Env, kC, k.Environment, "environment", nil)
 	if err != nil {
 		return err
 	}
 
 	k.Interfaces = make(map[string]interface{})
-	err = GetStringMap(m.Interfaces, kC.Interfaces, k.Interfaces, "interfaces", nil)
+	err = GetStringMap(m.Interfaces, kC, k.Interfaces, "interfaces", nil)
 	if err != nil {
 		return err
 	}
 
-	err = GetFrontend(m.Frontend, kC.Frontend, &k.Frontend)
+	err = GetFrontend(m.Frontend, kC, &k.Frontend)
 	if err != nil {
 		return err
 	}
 
-	err = GetStringSlice(m.Imports, kC.Imports, &k.Imports, "imports")
+	err = GetStringSlice(m.Imports, kC, &k.Imports, "imports")
 	if err != nil {
 		return err
 	}
