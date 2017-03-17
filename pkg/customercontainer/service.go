7b32d59bc46c2e402b387eb6ec233d23ba5c5a16
Dennis Heinze <ttdennis@users.noreply.github.com>
2017-03-17 14:14:21 +0100

    Implement CreateDockerImage function

--- pkg/customercontainer/service.go
+++ pkg/customercontainer/service.go
@@ -2,15 +2,23 @@
 package customercontainer
 
 import (
+	"archive/tar"
 	"bytes"
 	"context"
 	"encoding/json"
 	"fmt"
+	"io"
+	"os"
+	"path/filepath"
+	"regexp"
 	"strings"
 
 	"github.com/docker/docker/api/types"
 	"github.com/docker/docker/api/types/container"
 	"github.com/docker/docker/api/types/strslice"
+	"github.com/docker/docker/builder/dockerignore"
+	"github.com/docker/docker/cli/command/image/build"
+	"github.com/docker/docker/pkg/archive"
 	"github.com/ttdennis/kontainer.io/pkg/abstraction"
 	"github.com/ttdennis/kontainer.io/pkg/kmi"
 )
@@ -134,43 +142,151 @@ func (s *service) CreateDockerImage(refid int, kmi kmi.KMI) error {
 	labels := make(map[string]string)
 	labels["user"] = string(refid)
 
-	s.dcli.ImageBuild(context.Background(), nil, types.ImageBuildOptions{
+	dockerfile, err := addEnvToDockerfile(kmi.Dockerfile, kmi.Environment)
+	if err != nil {
+		return err
+	}
+
+	buildContext, err := createBuildContext(kmi.Container, dockerfile)
+	if err != nil {
+		return err
+	}
+
+	s.dcli.ImageBuild(context.Background(), buildContext, types.ImageBuildOptions{
 		Tags: []string{
 			fmt.Sprintf("%d-%s", refid, kmi.Name),
 		},
 		SuppressOutput: false,
-		RemoteContext: kmi.Context
-		NoCache:     true,
-		Remove:      false,
-		ForceRemove: false,
-		PullParent:  false,
-		CPUSetCPUs:  "1",
-		// CPUSetMems:  "1",
-		Memory:     0x1fffffff, // 500 MB
-		MemorySwap: 0x3fffffff, // 1 GB
-		// CgroupParent   string
-		// NetworkMode    string
-		// ShmSize        int64
-		Dockerfile: kmi.Dockerfile + kmi.Env,
-		// Ulimits        []*units.Ulimit
-		// BuildArgs needs to be a *string instead of just a string so that
-		// we can tell the difference between "" (empty string) and no value
-		// at all (nil). See the parsing of buildArgs in
-		// api/server/router/build/build_routes.go for even more info.
-		// BuildArgs   map[string]*string
-		// AuthConfigs map[string]AuthConfig
-		// Context     io.Reader
-		Labels: labels,
-		// squash the resulting image's layers to the parent
-		// preserves the original image and creates a new one from the parent with all
-		// the changes applied to a single layer
-		Squash: false, // TODO: Test disk impact
-		// CacheFrom specifies images that are used for matching cache. Images
-		// specified here do not need to have a valid parent chain to match cache.
-		// CacheFrom   []string
-		// ExtraHosts  []string // List of extra hosts
+		NoCache:        true,
+		Remove:         false,
+		ForceRemove:    false,
+		PullParent:     false,
+		CPUSetCPUs:     kmi.Resources["cpus"].(string),
+		Memory:         kmi.Resources["mem"].(int64),
+		MemorySwap:     kmi.Resources["swap"].(int64),
+		Dockerfile:     dockerfile,
+		Labels:         labels,
 	})
 	return nil
+}
+
+func createBuildContext(path string, dockerfileContent string) (io.Reader, error) {
+	var buildCtx io.ReadCloser
+
+	// Read dockerignore file from container path
+	f, err := os.Open(filepath.Join(path, ".dockerignore"))
+	if err != nil && !os.IsNotExist(err) {
+		return nil, err
+	}
+	defer f.Close()
+
+	var excludes []string
+	if err == nil {
+		excludes, err = dockerignore.ReadAll(f)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	if err := build.ValidateContextDirectory(path, excludes); err != nil {
+		return nil, fmt.Errorf("Cannot create context from container path: %s", err)
+	}
+
+	compression := archive.Uncompressed
+
+	// Create tar from build context
+	buildCtx, err = archive.TarWithOptions(path, &archive.TarOptions{
+		ExcludePatterns: excludes,
+		Compression:     compression,
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	// Add custom dockerfile to tar archive
+	buildCtx, err = addDockerfileToTar(buildCtx, dockerfileContent)
+	if err != nil {
+		return nil, err
+	}
+
+	return buildCtx, nil
+}
+
+func addDockerfileToTar(inputTar io.ReadCloser, dockerfileContent string) (io.ReadCloser, error) {
+	pipeReader, pipeWriter := io.Pipe()
+	tw := tar.NewWriter(pipeWriter)
+
+	// Open current archive for reading
+	tr := tar.NewReader(inputTar)
+
+	for {
+		hdr, err := tr.Next()
+
+		// End of archive
+		if err == io.EOF {
+			// Write dockerfile to tar
+			dockerfileBytes := []byte(dockerfileContent)
+			hdr = &tar.Header{
+				Name: "Dockerfile",
+				Size: int64(len(dockerfileBytes)),
+			}
+
+			if err := tw.WriteHeader(hdr); err != nil {
+				return nil, err
+			}
+
+			if _, err := tw.Write(dockerfileBytes); err != nil {
+				return nil, err
+			}
+
+			tw.Close()
+			return pipeReader, nil
+		}
+
+		if err != nil {
+			return nil, err
+		}
+
+		// Copy header from file
+		if err := tw.WriteHeader(hdr); err != nil {
+			return nil, err
+		}
+
+		// Copy contents of file
+		content := io.Reader(tr)
+		if _, err := io.Copy(tw, content); err != nil {
+			return nil, err
+		}
+
+	}
+}
+
+func addEnvToDockerfile(dockerfile string, env map[string]interface{}) (string, error) {
+	envString := "ENV"
+	for k, v := range env {
+		if envKeyValid(k) {
+			// To optimize performance all ENVs are put into one line
+			// so that docker only needs to create one container layer for
+			// all environment variables
+			envString = fmt.Sprintf("%s %s=\"%s\"", envString, k, envValueEscape(v.(string)))
+		} else {
+			return "", fmt.Errorf("Invalid ENV key (%s)", k)
+		}
+	}
+
+	return dockerfile, nil
+}
+
+func envKeyValid(envKey string) bool {
+	r, _ := regexp.Compile("^[a-zA-Z_][a-zA-Z0-9_]*$")
+	if r.MatchString(envKey) {
+		return true
+	}
+	return false
+}
+
+func envValueEscape(envValue string) string {
+	return strings.Replace(envValue, "\"", "\\\"", -1)
 }
 
 // NewService creates a customercontainer with necessary dependencies.
