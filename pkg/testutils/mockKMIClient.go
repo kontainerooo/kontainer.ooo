e34b794b986bbeff6aeeb93ea711cda4e405c0a3
Dennis Heinze <dennis.heinze@me.com>
2017-03-19 17:10:05 +0100

    Create mockKMIClient for testing

---
+++ pkg/testutils/mockKMIClient.go
@@ -0,0 +1,48 @@
+package testutils
+
+import (
+	"context"
+	"errors"
+
+	"github.com/go-kit/kit/log"
+	"github.com/ttdennis/kontainer.io/pkg/kmi"
+)
+
+// MockKMIClient simulates a kmi client for testing purposes
+type MockKMIClient struct {
+	kmis map[uint]kmi.KMI
+}
+
+// NewMockKMIEndpoints creates a new MockKMIClientEndpoints
+func NewMockKMIEndpoints(logger log.Logger, m MockKMIClient) *kmi.Endpoints {
+	return &kmi.Endpoints{
+		GetKMIEndpoint: m.GetKMIEndpoint,
+	}
+}
+
+// GetKMIEndpoint returns a given kmi
+func (m *MockKMIClient) GetKMIEndpoint(ctx context.Context, req interface{}) (interface{}, error) {
+	kmiReq := req.(*kmi.GetKMIRequest)
+
+	k, ok := m.kmis[kmiReq.ID]
+	if !ok {
+		return &kmi.GetKMIResponse{}, errors.New("KMI not found")
+	}
+
+	return &kmi.GetKMIResponse{
+		Error: nil,
+		KMI:   &k,
+	}, nil
+}
+
+// AddMockKMI adds a new KMI entry
+func (m *MockKMIClient) AddMockKMI(id uint, kmi kmi.KMI) {
+	m.kmis[id] = kmi
+}
+
+// NewMockKMIClient creates a new MockKMIClient
+func NewMockKMIClient() *MockKMIClient {
+	return &MockKMIClient{
+		kmis: make(map[uint]kmi.KMI),
+	}
+}
