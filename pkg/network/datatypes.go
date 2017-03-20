301161e7fdba544cca4299a6d0e60082a5d3eb4f
Dennis Heinze <dennis.heinze@me.com>
2017-03-20 10:13:17 +0100

    Adapt network datatypes

--- pkg/network/datatypes.go
+++ pkg/network/datatypes.go
@@ -3,9 +3,14 @@ package network
 
 // Networks stores the networks belonging to a user
 type Networks struct {
-	UserID    uint
-	Name      string
-	NetworkID string
+	UserID   uint
+	Networks []ID
+}
+
+// ID stores network's names and their respective IDs
+type ID struct {
+	Name string
+	ID   string
 }
 
 // Config describes configuration options for Networks
