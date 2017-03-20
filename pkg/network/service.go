98a43ca661e3a5a829b963570a4241217dd2b8cf
Dennis Heinze <dennis.heinze@me.com>
2017-03-20 10:10:06 +0100

    Create network service envelope

--- pkg/network/service.go
+++ pkg/network/service.go
@@ -1,6 +1,12 @@
 // Package network handles container networks and interconnections
 package network
 
+import (
+	"log"
+
+	"github.com/ttdennis/kontainer.io/pkg/abstraction"
+)
+
 // Service NetworkService
 type Service interface {
 	// CreateNetwork creates a new network for a given user and returns its ID and name
@@ -16,8 +22,50 @@ type Service interface {
 	RemoveContainerFromNetwork(refid int, name string, containerID string) error
 
 	// ExposePortToContainer exposes a port from one container to another
-	ExposePortToContainer(refid int, srcContainerID string, port uint32, destContainerID string) (int, error)
+	ExposePortToContainer(refid int, srcContainerID string, port uint32, destContainerID string) error
 
 	// RemovePortFromContainer removes an exposed port from a container
 	RemovePortFromContainer(refid int, srcContainerID string, port uint32, destContainerID string) error
+}
+
+type service struct {
+	dcli   abstraction.DCli
+	logger log.Logger
+}
+
+func (s *service) CreateNetwork(refid int, cfg *Config) (name string, id string, err error) {
+	// TODO: implement
+	return "", "", nil
+}
+
+func (s *service) RemoveNetworkByName(refid int, name string) error {
+	// TODO: implement
+	return nil
+}
+
+func (s *service) AddContainerToNetwork(refid int, name string, containerID string) error {
+	// TODO: implement
+	return nil
+}
+
+func (s *service) RemoveContainerFromNetwork(refid int, name string, containerID string) error {
+	// TODO: implement
+	return nil
+}
+
+func (s *service) ExposePortToContainer(refid int, srcContainerID string, port uint32, destContainerID string) error {
+	// TODO: implement
+	return nil
+}
+
+func (s *service) RemovePortFromContainer(refid int, srcContainerID string, port uint32, destContainerID string) error {
+	// TODO: implement
+	return nil
+}
+
+// NewService creates a new network service
+func NewService(dcli abstraction.DCli) Service {
+	return &service{
+		dcli: dcli,
+	}
 }
