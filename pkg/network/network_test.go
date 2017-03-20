d79fcab4d80548966cdf113ca994490682d9c927
Dennis Heinze <dennis.heinze@me.com>
2017-03-20 11:16:09 +0100

    Create NetworkCreate tests

--- pkg/network/network_test.go
+++ pkg/network/network_test.go
@@ -23,4 +23,47 @@ var _ = Describe("Network", func() {
 			Ω(err).Should(HaveOccurred())
 		})
 	})
+
+	Describe("Create Network", func() {
+		dcli := testutils.NewMockDCli()
+		db := testutils.NewMockDB()
+		networkService, _ := network.NewService(dcli, db)
+		It("Should create a new network", func() {
+			name, id, err := networkService.CreateNetwork(123, &network.Config{
+				Driver: "bridge",
+			})
+
+			Ω(err).ShouldNot(HaveOccurred())
+			Ω(id).ShouldNot(BeEmpty())
+			Ω(name).ShouldNot(BeEmpty())
+
+			Ω(len(dcli.GetNetworks())).Should(Equal(1))
+		})
+
+		It("Should error on client network generation", func() {
+			dcli.SetError()
+			name, id, err := networkService.CreateNetwork(123, &network.Config{
+				Driver: "bridge",
+			})
+
+			Ω(err).Should(HaveOccurred())
+			Ω(id).Should(BeEmpty())
+			Ω(name).Should(BeEmpty())
+
+			Ω(len(dcli.GetNetworks())).Should(Equal(1))
+		})
+
+		It("Should receive a database error", func() {
+			db.SetError(1)
+			name, id, err := networkService.CreateNetwork(123, &network.Config{
+				Driver: "bridge",
+			})
+
+			Ω(err).Should(HaveOccurred())
+			Ω(id).Should(BeEmpty())
+			Ω(name).Should(BeEmpty())
+
+			Ω(len(dcli.GetNetworks())).Should(Equal(1))
+		})
+	})
 })
