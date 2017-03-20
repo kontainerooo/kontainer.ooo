dc452213ab7a22920ff8698b9e1435d9b0b79ea2
Dennis Heinze <ttdennis@users.noreply.github.com>
2017-03-20 10:48:16 +0100

    Add network service test

--- pkg/network/network_test.go
+++ pkg/network/network_test.go
@@ -2,8 +2,25 @@ package network_test
 
 import (
 	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+
+	"github.com/ttdennis/kontainer.io/pkg/network"
+	"github.com/ttdennis/kontainer.io/pkg/testutils"
 )
 
 var _ = Describe("Network", func() {
+	Describe("Create service", func() {
+		It("Should create service", func() {
+			networkService, err := network.NewService(testutils.NewMockDCli(), testutils.NewMockDB())
+			Ω(networkService).ShouldNot(BeNil())
+			Ω(err).ShouldNot(HaveOccurred())
+		})
 
+		It("Should return db error", func() {
+			db := testutils.NewMockDB()
+			db.SetError(1)
+			_, err := network.NewService(testutils.NewMockDCli(), db)
+			Ω(err).Should(HaveOccurred())
+		})
+	})
 })
