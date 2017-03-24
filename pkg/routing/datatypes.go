aec786176f168f0dc0cf0cb9bc3bb69cb17189ba
Tom Burmeister <tobuto@users.noreply.github.com>
2017-03-24 19:15:37 +0100

    Fix Routing Service Database Types

--- pkg/routing/datatypes.go
+++ pkg/routing/datatypes.go
@@ -17,10 +17,68 @@ type ListenStatement struct {
 	keyword   string
 }
 
+// Scan implements the sql.Scanner interface.
+func (l *ListenStatement) Scan(src interface{}) error {
+	switch src := src.(type) {
+	case []byte:
+		return l.scanBytes(src)
+	case string:
+		return l.scanBytes([]byte(src))
+	case nil:
+		*l = ListenStatement{}
+		return nil
+	}
+
+	return fmt.Errorf("pq: cannot convert %T to Frontend Array", src)
+}
+
+func (l *ListenStatement) scanBytes(src []byte) error {
+	return json.Unmarshal(src, l)
+}
+
+// Value implements the driver.Valuer interface.
+func (l ListenStatement) Value() (driver.Value, error) {
+	if l == (ListenStatement{}) {
+		return nil, nil
+	}
+	b, err := json.Marshal(l)
+
+	return string(b), err
+}
+
 // Log combines a path and a keyword
 type Log struct {
 	path    string
 	keyword string
+}
+
+// Scan implements the sql.Scanner interface.
+func (l *Log) Scan(src interface{}) error {
+	switch src := src.(type) {
+	case []byte:
+		return l.scanBytes(src)
+	case string:
+		return l.scanBytes([]byte(src))
+	case nil:
+		*l = Log{}
+		return nil
+	}
+
+	return fmt.Errorf("pq: cannot convert %T to Frontend Array", src)
+}
+
+func (l *Log) scanBytes(src []byte) error {
+	return json.Unmarshal(src, l)
+}
+
+// Value implements the driver.Valuer interface.
+func (l Log) Value() (driver.Value, error) {
+	if l == (Log{}) {
+		return nil, nil
+	}
+	b, err := json.Marshal(l)
+
+	return string(b), err
 }
 
 // SSLSettings represents a set of configurations in terms of ssl transport
@@ -34,8 +92,39 @@ type SSLSettings struct {
 
 // LocationRule is a struct which combines a single location (URL path) with a set of rules
 type LocationRule struct {
-	location string
-	rules    map[string][]string
+	Location string
+	Rules    map[string][]string
+}
+
+// Scan implements the sql.Scanner interface.
+func (l *LocationRule) Scan(src interface{}) error {
+	switch src := src.(type) {
+	case []byte:
+		return l.scanBytes(src)
+	case string:
+		return l.scanBytes([]byte(src))
+	case nil:
+		*l = LocationRule{
+			Rules: make(map[string][]string),
+		}
+		return nil
+	}
+
+	return fmt.Errorf("pq: cannot convert %T to Frontend Array", src)
+}
+
+func (l *LocationRule) scanBytes(src []byte) error {
+	return json.Unmarshal(src, l)
+}
+
+// Value implements the driver.Valuer interface.
+func (l LocationRule) Value() (driver.Value, error) {
+	if l.Location == "" {
+		return nil, nil
+	}
+	b, err := json.Marshal(l)
+
+	return string(b), err
 }
 
 // LocationRules is an array of LocationRules
@@ -72,18 +161,18 @@ func (l LocationRules) Value() (driver.Value, error) {
 
 // The RouterConfig struct represents the collected information needed to configurate an http router
 type RouterConfig struct {
-	RefID           uint   `gorm:"primary_key"`
-	Name            string `gorm:"primary_key"`
-	ListenStatement ListenStatement
-	ServerName      pq.StringArray
-	AccessLog       Log
-	ErrorLog        Log
-	EootPath        string
-	SSLSettings     SSLSettings
-	LocationRules   LocationRules
+	RefID           uint            `gorm:"primary_key"`
+	Name            string          `gorm:"primary_key"`
+	ListenStatement ListenStatement `sql:"type:jsonb"`
+	ServerName      pq.StringArray  `sql:"type:text[]"`
+	AccessLog       Log             `sql:"type:jsonb"`
+	ErrorLog        Log             `sql:"type:jsonb"`
+	RootPath        string
+	SSLSettings     SSLSettings   `sql:"type:jsonb"`
+	LocationRules   LocationRules `sql:"type:jsonb[]"`
 }
 
 // TableName sets RouterConfig's database table name
 func (RouterConfig) TableName() string {
-	return "Routing"
+	return "routing"
 }
