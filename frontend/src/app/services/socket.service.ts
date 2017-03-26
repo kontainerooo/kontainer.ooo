0acd7a7be22518d0ec99291909bff19043d9652c
Marvin König <kingmarv@users.noreply.github.com>
2017-03-26 20:57:51 +0200

    Add basic socket service and user service
    This commit adds a basic services to connect to the backend, namely
    SocketService and UserService. For this matter protobufjs has been added
    and the TypeScript version was bumped to ~2.2.0.
    
    Furthermore, the protobuf packages have been changed while keeping them
    intact for the go code with the option go_package.
    
    A declaration file for the protocol.json file has also been added in
    order to import it in TypeScript.

--- frontend/src/app/services/socket.service.ts
+++ frontend/src/app/services/socket.service.ts
@@ -1,11 +1,18 @@
 import { Injectable } from '@angular/core';
 import * as Rx from 'rxjs/Rx';
+import * as pb from '../../messages/messages';
+import { OpcodeConverter } from '../classes/opcode-converter';
 
 @Injectable()
 export class SocketService {
   private socket: Rx.Subject<MessageEvent>;
+  private opcodeConverter: OpcodeConverter;
+  private readonly PROTOCOL_VERSION: string;
 
-  constructor() { }
+  constructor() { 
+    this.opcodeConverter = new OpcodeConverter();
+    this.PROTOCOL_VERSION = 'kroov1';
+  }
 
   public connect(url): Rx.Subject<MessageEvent> {
     if(!this.socket) {
@@ -16,9 +23,11 @@ export class SocketService {
   }
 
   private create(url): Rx.Subject<MessageEvent> {
-    let ws = new WebSocket(url);
+    // TODO add protocol when supported
+    let ws = new WebSocket(url/*, this.PROTOCOL_VERSION*/);
+    ws.binaryType = 'arraybuffer';
 
-    let observable = Rx.Observable.create(
+    let observable: Rx.Observable<MessageEvent> = Rx.Observable.create(
       (obs: Rx.Observer<MessageEvent>) => {
         ws.onmessage = obs.next.bind(obs);
         ws.onerror = obs.error.bind(obs);
@@ -29,9 +38,9 @@ export class SocketService {
     );
 
     let observer = {
-      next: (data: Object) => {
+      next: (data: Uint8Array) => {
         if (ws.readyState === WebSocket.OPEN) {
-          ws.send(JSON.stringify(data));
+          ws.send(data);
         }
       }
     };
@@ -39,4 +48,44 @@ export class SocketService {
     return Rx.Subject.create(observer, observable);
   }
 
+  private generateOpcode(opcode: string): Array<number> {
+    const opcodeLength: number = 3;
+    if (opcode.length === opcodeLength) {
+      let opcodeArray: Array<number> = new Array<number>();
+      for (let i = 0; i < opcodeLength; i++) {
+        opcodeArray[i] = opcode.charCodeAt(i);
+      }
+      return opcodeArray;
+    }
+    return null;
+  }
+
+  public encodeMessage(pkg: string, message: string, data: object): Uint8Array {
+    const opcodes: { pkg: string, message: string } = this.opcodeConverter.getOpcodes(pkg, message.replace(/Request|Response/, ''));
+    const pkgArray: Array<number> = this.generateOpcode(opcodes.pkg);
+    const messageArray: Array<number> = this.generateOpcode(opcodes.message);
+    const dataArray: Array<number> = Array.from(<Uint8Array>pb[pkg][message].encode(data).finish());
+    if (pkgArray !== null && messageArray !== null) {
+      return new Uint8Array([...pkgArray, ...messageArray, ...dataArray]);
+    }
+  }
+
+  public decodeMessage(encodedMessage: Uint8Array, pkgWanted: string): object {
+    console.log(encodedMessage);
+    const pkgArray: Uint8Array = encodedMessage.slice(0, 3);
+    const messageArray: Uint8Array = encodedMessage.slice(3, 6);
+    let pkg: string = '';
+    let message: string = '';
+    for(let i in pkgArray) {
+      pkg += String.fromCharCode(pkgArray[i]);
+      message += String.fromCharCode(messageArray[i]);
+    }
+    console.log(pkg, message);
+    const identifiers: { pkg: string, message: string } = this.opcodeConverter.getIdentifiers(pkg, message);
+    if(identifiers.pkg == pkgWanted) {
+      return pb[identifiers.pkg][`${identifiers.message}Response`].decode(encodedMessage.slice(6)).toObject();
+    } else {
+      return null;
+    }
+  }
 }
