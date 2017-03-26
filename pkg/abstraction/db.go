704480e569d1d2941b7e713cdc715d822a302ecd
Tom Burmeister <tobuto@users.noreply.github.com>
2017-03-26 14:53:22 +0200

    Add RemoveFromArray function in DB abstraction

--- pkg/abstraction/db.go
+++ pkg/abstraction/db.go
@@ -34,6 +34,7 @@ type DB interface {
 	GetValue() interface{}
 
 	AppendToArray(query interface{}, target string, values interface{}) error
+	RemoveFromArray(query interface{}, target string, index int) error
 
 	// Every function below invokes gorm.DB's function and returns gorm.DB's Error property
 	Begin()
@@ -61,61 +62,22 @@ func (w *dbWrapper) GetValue() interface{} {
 	return w.db.Value
 }
 
-func (w *dbWrapper) AppendToArray(elem interface{}, target string, value interface{}) error {
-	var (
-		tableName string
-		stringVal string
-		primary   []string
-		query     string
-		typeCast  string
-	)
-
-	v := reflect.ValueOf(elem)
-	if v.Kind() == reflect.Ptr {
-		v = v.Elem()
-	}
+func (w *dbWrapper) getTableName(v reflect.Value) string {
 	t := v.Type()
-
-	// check if target is field of reference type
-	f, isPart := t.FieldByName(strings.Title(target))
-	if !isPart {
-		return fmt.Errorf("%s is not a column in destination table", target)
-	}
-
-	tag := f.Tag.Get("sql")
-	re := regexp.MustCompile(`type:(.+)\[\]`)
-	match := re.FindStringSubmatch(tag)
-	if len(match) < 2 {
-		return fmt.Errorf("unsupported type %s", tag)
-	}
-	typeCast = match[1]
 
 	// Get TableName, either from type name or if it exists from TableName function
 	nameFunc, exists := t.MethodByName("TableName")
 	if exists {
 		nameResult := nameFunc.Func.Call([]reflect.Value{v})
-		tableName = nameResult[0].String()
-	} else {
-		tableName = gorm.ToDBName(t.Name())
+		return nameResult[0].String()
 	}
+	return gorm.ToDBName(t.Name())
+}
 
-	valuer, exists := reflect.TypeOf(value).MethodByName("Value")
+func (w *dbWrapper) generateWhereClause(query *string, v reflect.Value) error {
+	var primary []string
 
-	if exists {
-		valuerResult := valuer.Func.Call([]reflect.Value{reflect.ValueOf(value)})
-		stringVal = valuerResult[0].Interface().(string)
-	} else {
-		var ok bool
-		stringVal, ok = value.(string)
-		if !ok {
-			return errors.New("type not supported")
-		}
-	}
-
-	target = gorm.ToDBName(target)
-
-	// Set Update parameters
-	query = fmt.Sprintf("UPDATE %s SET %s = %s || $1::%s", tableName, target, target, typeCast)
+	t := v.Type()
 
 	// Search for primary keys in the reference type
 	idTag := "ID"
@@ -141,31 +103,120 @@ func (w *dbWrapper) AppendToArray(elem interface{}, target string, value interfa
 	}
 
 	// Set WHERE clause
-	query = fmt.Sprintf("%s WHERE", query)
+	*query = fmt.Sprintf("%s WHERE", *query)
 	for i, key := range primary {
 		if i > 0 {
-			query = fmt.Sprintf("%s AND", query)
+			*query = fmt.Sprintf("%s AND", *query)
 		}
 		value := v.FieldByName(key)
 		key = gorm.ToDBName(key)
 		k := value.Kind()
 		if k == reflect.String {
-			query = fmt.Sprintf("%s %s='%s'", query, key, value.String())
+			*query = fmt.Sprintf("%s %s='%s'", *query, key, value.String())
 		} else if k == reflect.Uint {
-			query = fmt.Sprintf("%s %s=%d", query, key, value.Uint())
+			*query = fmt.Sprintf("%s %s=%d", *query, key, value.Uint())
 		} else {
 			return fmt.Errorf("unexpected %s", k)
 		}
 
 	}
-	query = fmt.Sprintf("%s RETURNING cardinality(%s) AS index", query, target)
 
-	if w.tx != nil {
-		_, err := w.tx.CommonDB().Exec(query, stringVal)
+	return nil
+}
+
+func (w *dbWrapper) AppendToArray(elem interface{}, target string, value interface{}) error {
+	var (
+		tableName string
+		stringVal string
+		query     string
+		typeCast  string
+	)
+
+	v := reflect.ValueOf(elem)
+	if v.Kind() == reflect.Ptr {
+		v = v.Elem()
+	}
+	t := v.Type()
+
+	// check if target is field of reference type
+	f, isPart := t.FieldByName(strings.Title(target))
+	if !isPart {
+		return fmt.Errorf("%s is not a column in destination table", target)
+	}
+	target = gorm.ToDBName(target)
+
+	tag := f.Tag.Get("sql")
+	re := regexp.MustCompile(`type:(.+)\[\]`)
+	match := re.FindStringSubmatch(tag)
+	if len(match) < 2 {
+		return fmt.Errorf("unsupported type %s", tag)
+	}
+	typeCast = match[1]
+
+	tableName = w.getTableName(v)
+
+	valuer, exists := reflect.TypeOf(value).MethodByName("Value")
+
+	if exists {
+		valuerResult := valuer.Func.Call([]reflect.Value{reflect.ValueOf(value)})
+		stringVal = valuerResult[0].Interface().(string)
+	} else {
+		var ok bool
+		stringVal, ok = value.(string)
+		if !ok {
+			return errors.New("type not supported")
+		}
+	}
+
+	// Set Update parameters
+	query = fmt.Sprintf("UPDATE %s SET %s = %s || $1::%s", tableName, target, target, typeCast)
+
+	err := w.generateWhereClause(&query, v)
+	if err != nil {
 		return err
 	}
 
-	_, err := w.db.DB().Exec(query, stringVal)
+	query = fmt.Sprintf("%s RETURNING cardinality(%s) AS index", query, target)
+
+	if w.tx != nil {
+		_, err = w.tx.CommonDB().Exec(query, stringVal)
+		return err
+	}
+
+	_, err = w.db.DB().Exec(query, stringVal)
+	return err
+}
+
+func (w *dbWrapper) RemoveFromArray(elem interface{}, target string, index int) error {
+	var query string
+
+	v := reflect.ValueOf(elem)
+	if v.Kind() == reflect.Ptr {
+		v = v.Elem()
+	}
+	t := v.Type()
+
+	_, isPart := t.FieldByName(strings.Title(target))
+	if !isPart {
+		return fmt.Errorf("%s is not a column in destination table", target)
+	}
+	target = gorm.ToDBName(target)
+
+	tableName := w.getTableName(v)
+
+	query = fmt.Sprintf("UPDATE %s SET %s = array_remove(%s, %s[$1])", tableName, target, target, target)
+
+	err := w.generateWhereClause(&query, v)
+	if err != nil {
+		return err
+	}
+
+	if w.tx != nil {
+		_, err = w.tx.CommonDB().Exec(query, index)
+		return err
+	}
+
+	_, err = w.db.DB().Exec(query, index)
 	return err
 }
 
