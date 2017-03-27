ffcf931b48d579213da9fe6ab46fc8313c0bd91c
Dennis Heinze <dennis.heinze@me.com>
2017-03-27 09:40:39 +0200

    Use Inet type and create networkInterface struct

--- pkg/firewall/service.go
+++ pkg/firewall/service.go
@@ -1,64 +1,66 @@
 // Package firewall handles the firewall and forwarding configuration
 package firewall
 
-// Service IPTablesService
+import "github.com/kontainerooo/kontainer.ooo/pkg/abstraction"
+
+// Service firewall
 type Service interface {
 
 	// InitBridge initializes a bridge network
-	InitBridge(ip string) error
+	InitBridge(ip abstraction.Inet, netIf string) error
 
 	// AllowConnection sets up a rule to let src talk to dst
-	AllowConnection(src string, dst string) error
+	AllowConnection(src abstraction.Inet, dst abstraction.Inet) error
 
 	// AllowConnection sets up a rule to block src from talking to dst
-	BlockConnection(src string, dst string) error
+	BlockConnection(src abstraction.Inet, dst abstraction.Inet) error
 
 	// AllowPort sets up a rule to let src talk to dst on port port
-	AllowPort(src string, dst string, port uint32) error
+	AllowPort(src abstraction.Inet, dst abstraction.Inet, port uint32) error
 
 	// AllowPort sets up a rule to block src from talking to dst on port port
-	BlockPort(src string, dst string, port uint32) error
+	BlockPort(src abstraction.Inet, dst abstraction.Inet, port uint32) error
 
 	// RedirectPort redirects the src port to the dst port on ip
-	RedirectPort(ip string, src uint32, dst uint32) error
+	RedirectPort(ip abstraction.Inet, src uint32, dst uint32) error
 
 	// RemoveRedirectPort removes a port redirection
-	RemoveRedirectPort(ip string, src uint32, dst uint32) error
+	RemoveRedirectPort(ip abstraction.Inet, src uint32, dst uint32) error
 }
 
 type service struct{}
 
-func (s *service) InitBridge(ip string) error {
+func (s *service) InitBridge(ip abstraction.Inet, netIf string) error {
 	// TODO: implement
 	return nil
 }
 
-func (s *service) AllowConnection(src string, dst string) error {
+func (s *service) AllowConnection(src abstraction.Inet, dst abstraction.Inet) error {
 	// TODO: implement
 	return nil
 }
 
-func (s *service) BlockConnection(src string, dst string) error {
+func (s *service) BlockConnection(src abstraction.Inet, dst abstraction.Inet) error {
 	// TODO: implement
 	return nil
 }
 
-func (s *service) AllowPort(src string, dst string, port uint32) error {
+func (s *service) AllowPort(src abstraction.Inet, dst abstraction.Inet, port uint32) error {
 	// TODO: implement
 	return nil
 }
 
-func (s *service) BlockPort(src string, dst string, port uint32) error {
+func (s *service) BlockPort(src abstraction.Inet, dst abstraction.Inet, port uint32) error {
 	// TODO: implement
 	return nil
 }
 
-func (s *service) RedirectPort(ip string, src uint32, dst uint32) error {
+func (s *service) RedirectPort(ip abstraction.Inet, src uint32, dst uint32) error {
 	// TODO: implement
 	return nil
 }
 
-func (s *service) RemoveRedirectPort(ip string, src uint32, dst uint32) error {
+func (s *service) RemoveRedirectPort(ip abstraction.Inet, src uint32, dst uint32) error {
 	// TODO: implement
 	return nil
 }
