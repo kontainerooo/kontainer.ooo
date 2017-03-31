84a0a0498f573d71e56e65a668e8068c4be3fca9
Dennis Heinze <dennis.heinze@me.com>
2017-03-31 13:48:07 +0200

    Introduce table to store networks and their connected containers

--- pkg/network/datatypes.go
+++ pkg/network/datatypes.go
@@ -1,11 +1,21 @@
 // Package network handles container networks and interconnections
 package network
 
+import "github.com/kontainerooo/kontainer.ooo/pkg/abstraction"
+
 // Networks stores the networks belonging to a user
 type Networks struct {
 	UserID      uint
 	NetworkID   string `gorm:"primary_key"`
 	NetworkName string
+	IsPrimary   bool
+}
+
+// Containers map containers to their networks
+type Containers struct {
+	NetworkID   string
+	ContainerID string
+	ContainerIP abstraction.Inet `gorm:"primary_key"`
 }
 
 // Config describes configuration options for Networks
