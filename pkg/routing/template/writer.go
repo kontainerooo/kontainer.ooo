20ae981a05faba652e44647b89d32e633aa5a621
Tom Burmeister <tobuto@users.noreply.github.com>
2017-04-03 13:18:04 +0200

    Fix template bug in writer

--- pkg/routing/template/writer.go
+++ pkg/routing/template/writer.go
@@ -2,7 +2,9 @@ package template
 
 import (
 	"errors"
+	"fmt"
 	"os"
+	"strings"
 	"text/template"
 
 	"github.com/kontainerooo/kontainer.ooo/pkg/routing"
@@ -28,15 +30,37 @@ type Writer interface {
 type writer struct {
 	template *template.Template
 	path     string
+	name     string
 }
 
 func (w writer) CreateFile(c *routing.RouterConfig) error {
+	path := fmt.Sprintf("%s/%d_%s.conf", w.path, c.RefID, c.Name)
+
+	f, err := os.Create(path)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+
+	err = w.template.ExecuteTemplate(f, w.name, c)
+	if err != nil {
+		return err
+	}
+
 	return nil
 }
 
 // NewWriter returns a new writer
 func NewWriter(r Router, p string) (Writer, error) {
-	t, err := template.ParseFiles(router[r])
+	t := template.New("routing").Funcs(template.FuncMap{
+		"join": strings.Join,
+	})
+
+	if !(int(r) < len(router)) {
+		return nil, fmt.Errorf("Router with id %d does not exist", r)
+	}
+	name := router[r]
+	t, err := t.ParseFiles(name)
 	if err != nil {
 		return nil, err
 	}
@@ -51,5 +75,6 @@ func NewWriter(r Router, p string) (Writer, error) {
 	return &writer{
 		template: t,
 		path:     p,
+		name:     name,
 	}, nil
 }
