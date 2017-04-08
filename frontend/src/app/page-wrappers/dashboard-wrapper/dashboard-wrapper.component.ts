40e65158da00605682dd8690a5ea5876230df791
Marvin König <kingmarv@users.noreply.github.com>
2017-04-08 02:19:09 +0200

    Add unfinished dashboard component to routing
    A dashboard placeholder has been added to the dashboard wrapper routing.

---
+++ frontend/src/app/page-wrappers/dashboard-wrapper/dashboard-wrapper.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'kro-dashboard-wrapper',
+  templateUrl: './dashboard-wrapper.component.html',
+  styleUrls: ['./dashboard-wrapper.component.scss']
+})
+export class DashboardWrapperComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
