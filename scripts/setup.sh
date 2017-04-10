c6cac1a44807fcd0e2a33248decb94651398e00f
Dennis Heinze <dennis.heinze@me.com>
2017-04-10 21:34:37 +0200

    Install postgres 9.6 and add kroo user

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -14,11 +14,17 @@ add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
 
 # Install go and docker dependencies
-apt-get update
+apt-get update -y
 apt-get install linux-headers-3.13.0-116-generic apt-transport-https ca-certificates curl \
   software-properties-common linux-image-extra-$(uname -r) \
-  linux-image-extra-virtual docker-ce zip unzip postgresql postgresql-client -y
+  linux-image-extra-virtual docker-ce zip unzip postgresql-client -y
 sudo apt-get --no-install-recommends install -y virtualbox-guest-utils
+
+# Install postgres
+add-apt-repository "deb http://apt.postgresql.org/pub/repos/apt/ trusty-pgdg main"
+wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | apt-key add -
+apt-get update -y
+apt-get install postgresql-9.6 -y
 
 # Install go
 curl -O https://storage.googleapis.com/golang/go1.8.linux-amd64.tar.gz
@@ -43,6 +49,12 @@ chown vagrant /bin/protoc
 # Install npm
 rm -rf $USERDIR/.nvm && git clone https://github.com/creationix/nvm.git $USERDIR/.nvm && (cd $USERDIR/.nvm && git checkout `git describe --abbrev=0 --tags`) && . $USERDIR/.nvm/nvm.sh
 echo ". ~/.nvm/nvm.sh" >> $USERDIR/.profile
+
+# Set localhost to have hostname postgres (will change later)
+echo "127.0.0.1 postgres" >> /etc/hosts
+
+# Create postgres user
+sudo -u postgres bash -c "psql -c \"CREATE USER kroo WITH PASSWORD 'kroo';\""
 
 chown -R vagrant /var/go
 chown -R vagrant /home/vagrant/.nvm
