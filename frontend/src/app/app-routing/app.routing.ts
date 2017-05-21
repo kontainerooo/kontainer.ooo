b5ac0e5e04774541a1a71e99722295a4290ab9f1
Marvin König <kingmarv@users.noreply.github.com>
2017-05-21 16:06:36 +0200

    Add register route
    The settings component has also been added to the register route.

--- frontend/src/app/app-routing/app.routing.ts
+++ frontend/src/app/app-routing/app.routing.ts
@@ -1,12 +1,17 @@
 import { ModuleWithProviders } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { SignInComponent } from '../sign-in/sign-in.component';
+import { SettingsComponent } from '../pages/user/settings/settings.component';
 import { PageNotFoundComponent } from '../errors/page-not-found/page-not-found.component';
 
 const appRoutes: Routes = [
   {
     path: 'sign-in',
     component: SignInComponent
+  },
+  {
+    path: 'register',
+    component: SettingsComponent
   },
   {
     path: '',
