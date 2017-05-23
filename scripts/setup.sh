b66a12bed7730f959573f84faff94706efb8c82e
Dennis Heinze <ttdennis@users.noreply.github.com>
2017-05-23 09:41:56 +0200

    Remove old login with docker and adapt setup

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -7,7 +7,7 @@ GOPATH=/var/go/src
 add-apt-repository "deb http://apt.postgresql.org/pub/repos/apt/ trusty-pgdg main"
 wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | apt-key add -
 apt-get update -y
-apt-get install postgresql-9.6 -y
+apt-get install postgresql-9.6 git -y
 
 # Install go
 curl -O https://storage.googleapis.com/golang/go1.8.linux-amd64.tar.gz
@@ -39,5 +39,15 @@ echo "127.0.0.1 postgres" >> /etc/hosts
 # Create postgres user
 sudo -u postgres bash -c "psql -c \"CREATE USER kroo WITH PASSWORD 'kroo';\""
 
+# Install netns
+mkdir -p /var/lib/kontainerooo/images
+curl [REDACTED] -O /var/lib/kontainerooo/images/rootfs.tar
+
+mkdir -p /var/lib/kontainerooo/kmi
+cp /var/go/src/github.com/kontainerooo/kontainer.ooo/pkg/kmi/test.kmi /var/lib/kontainerooo/kmi
+
+go get github.com/jessfraz/netns
+
 chown -R vagrant /var/go
 chown -R vagrant /home/vagrant/.nvm
+chown -R vagrant /var/lib/kontainerooo
