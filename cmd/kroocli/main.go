f56cbd27a2e59aa8fb7b1bc1e9de7b2f9580dccc
tobuto <tobuto@users.noreply.github.com>
2017-05-25 19:35:33 +0200

    Add kroocli main.go in cmd folder

---
+++ cmd/kroocli/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"time"
+
+	"github.com/abiosoft/ishell"
+	"github.com/go-kit/kit/log"
+	"github.com/kontainerooo/kontainer.ooo/pkg/cli"
+	"google.golang.org/grpc"
+)
+
+func main() {
+	var (
+		grpcAddr = ":8082"
+	)
+
+	shell := ishell.New()
+	shell.Println("Kontainer.ooo interactive shell")
+
+	logger := log.NewLogfmtLogger(os.Stdout)
+
+	conn, err := grpc.Dial(grpcAddr, grpc.WithInsecure(), grpc.WithTimeout(time.Second))
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "error: %v", err)
+		os.Exit(1)
+	}
+	defer conn.Close()
+
+	cli.InitShell(shell, conn, logger)
+
+	shell.Start()
+}
