d58700e8b0d93d628556b91747ca44e2ad44ec73
tobuto <tobuto@users.noreply.github.com>
2017-05-30 16:47:39 +0200

    Catch session.Save error in TokenAuth mux

--- pkg/websocket/authentication.go
+++ pkg/websocket/authentication.go
@@ -92,7 +92,12 @@ func (t *tokenAuth) Mux(w http.ResponseWriter, r *http.Request) (interface{}, bo
 			session.Values[key.String()] = val.MapIndex(key)
 		}
 
-		session.Save(r, w)
+		err = session.Save(r, w)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return nil, true
+		}
+
 		w.Write([]byte("Authenticated!"))
 
 		return nil, true
@@ -160,6 +165,7 @@ func NewTokenAuth(
 	enc EncodeResponseFunc,
 	end endpoint.Endpoint,
 ) Authenticator {
+	// TODO: add validation of auth, encryptionKey
 	return &tokenAuth{
 		SessionStore: sessions.NewCookieStore([]byte(authenticationKey), []byte(encryptionKey)),
 		ServiceID:    serviceID,
